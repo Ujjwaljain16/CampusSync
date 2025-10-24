67dfc9b74d89de6dbcd110a9fb27aeb095645498
Ujjwaljain16 <jainujjwal1609@gmail.com>
2025-10-24 19:13:17 +0530

    Revert "Set favicon to logo.svg in layout"
    This reverts commit 5742a370ed4751abc313b2daac157b25eb9a0bf4.

--- my-app/database/check-vc-schema.sql
+++
@@ -1,9 +0,0 @@
--- Check verifiable_credentials table schema
-SELECT 
-    column_name, 
-    data_type, 
-    is_nullable,
-    column_default
-FROM information_schema.columns 
-WHERE table_name = 'verifiable_credentials'
-ORDER BY ordinal_position;
--- my-app/supabase-migrations/003_disable_rls_temporarily.sql
+++
@@ -1,8 +0,0 @@
--- Temporarily disable RLS to fix the recursion issue
--- This allows the system to work while we fix the policies
-
--- Disable RLS temporarily
-ALTER TABLE user_roles DISABLE ROW LEVEL SECURITY;
-
--- Note: In production, you should re-enable RLS with proper policies
--- For now, this allows the admin system to work
